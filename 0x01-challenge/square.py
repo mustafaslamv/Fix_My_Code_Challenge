00628b12618e434be58882c0612435deaf2f0a9c
mustafa slama <ayman1slama@gmail.com>
2023-12-16 16:29:41 +0200

    .

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -18,9 +18,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Object str representation """
         return "{}/{}".format(self.width, self.height)
 
 
