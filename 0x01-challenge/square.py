2feeaf3b05807e925918f8770c947ccd579eebe6
mustafa slama <ayman1slama@gmail.com>
2023-12-16 16:43:58 +0200

    /

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,6 +9,8 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
         self.height = self.width
